657dde7fc39c10dc8059d339f6ae73eb9abf0c08
MikeDCastillo <mikecastillouvu@gmail.com>
2017-06-12 01:49:18 -0500

    Adds Key File

---
+++ Chinese Medicine/Keys.swift
@@ -0,0 +1,23 @@
+//
+//  Keys.swift
+//  Chinese Medicine
+//
+//  Created by Michael Castillo on 6/12/17.
+//  Copyright © 2017 Michael Castillo. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+
+    static let americanName = "americanName"
+    static let description = "description"
+    static let etiology = "etiology"
+    static let id = "id"
+    static let image = "image"
+    static let pathologies = "pathologies"
+    static let pynyin = "pynyin"
+    static let tongueDescription = "tongueDescription"
+    static let tongueTypes = "tongueTypes"
+
+}
